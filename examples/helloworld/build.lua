048cd6f877b21f4b1b6b501cb227433655573ce2
Dylaris <3445818679@qq.com>
2025-05-02 16:29:04 +0800

    add debug, link, dynamic append function

--- examples/helloworld/build.lua
+++ examples/helloworld/build.lua
@@ -4,36 +4,30 @@ package.path = package.path .. ";" .. LUABC_DIR .. "?.lua"
 local luabc = require("luabc")
 local cmd   = luabc.cmd
 local tool  = luabc.tool
+local debug = luabc.debug
 
 local CC     = "gcc"
 local CFLAGS = { "-Wall", "-Wextra" }
 local CSTD   = "-std=c11"
 local TARGET = "helloworld"
 local SRC    = tool.match_file_extension("c")
-local OBJ    = (function () 
-    local res = {}
-    for _, file in ipairs(SRC) do
-        table.insert(res, tool.replace_file_extension(file, "o"))
-    end
-    return res
-end)()
+local OBJ    = tool.replace_files_extension(SRC, "o")
 local CLEAR_FILES = { TARGET, table.unpack(OBJ) }
 
 -- directly run the command
 -- usage: lua build.lua
---[[ 
+---[[ 
 local function build()
-    local cmd = cmd:new()
-    cmd:append(CC, CFLAGS, CSTD, "-o", TARGET, SRC)
-    cmd:run()
-    -- tool.clean(CLEAR_FILES)
+    local all = cmd:new()
+    all:append(CC, CFLAGS, CSTD, "-o", TARGET, SRC)
+    all:run(false)
 end
 build()
 --]]
 
 -- use label and bind
 -- usage: lua build.lua [-clean][-all]
----[[ 
+--[[ 
 local function build()
     -- execute the commands in ascending order
     -- cmd:new(label, order)
@@ -45,8 +39,9 @@ local function build()
     local link = cmd:new("all", #SRC+1)
     link:append(CC, CFLAGS, CSTD, "-o", TARGET, OBJ)
 
-    local clean = cmd:new("clean", 1)
-    clean:append("rm -rf", CLEAR_FILES)
+    local clean = cmd:new("clean")
+    -- clean:append("rm -rf", CLEAR_FILES)  -- run the command
+    clean:call(tool.clean, CLEAR_FILES)     -- run the function
 
     luabc.build()
 end
--- luabc.lua
+++ luabc.lua
@@ -13,95 +13,176 @@ local color = {
     WHITE  = "\x1b[37m",
 }
 
-local log = {
-    err = function (msg)
-        print("[" .. color.RED .. "ERROR" .. color.RESET .. "] " .. msg)
-    end,
-
-    ok = function (msg)
-        print("[" .. color.GREEN .. "OK" .. color.RESET .. "] " .. msg)
-    end,
-
-    info = function (msg)
-        print("[" .. color.CYAN .. "INFO" .. color.RESET .. "] " .. msg)
-    end,
-}
-
-
 local luabc = { 
-    bind = { ["__default"] = {} },
-    tool = {}, 
-    cmd = {} 
+    bind  = { ["__default"] = {} },     -- stores labels and their associated commands
+    tool  = {},                         -- contains useful utility functions
+    cmd   = {},                         -- defines functions related to command operations
+    debug = { log = {} }                -- contains functions and lgos for debugging
 }
 
-function luabc.build()
+-- @brief Build the entire project according the label (based on the cmd line input
+-- and recoreds in 'luabc.bind')
+-- @param is_clear Clear the old records after build or not (for debugging)
+function luabc.build(is_clear)
+    if is_clear == nil then is_clear = false else is_clear = is_clear and true end
+
     if #arg <= 1 then
         local label = arg[1] or "-__default"
         if string.sub(label, 1, 1) ~= "-" then
-            log.err("you should use label with '-': -" .. arg[1])
+            luabc.debug.log.err("you should use label with '-': -" .. arg[1])
             return
         end
         label = string.sub(label, 2, -1)
         if not luabc.bind[label] then
-            log.err("label "  .. label .. " is not binded")
+            luabc.debug.log.err("label "  .. label .. " is not binded")
         else
-            for _, cmd in ipairs(luabc.bind[label]) do cmd:run() end
+            for _, cmd in ipairs(luabc.bind[label]) do cmd:run(is_clear) end
         end
     else 
-        log.err("usage: lua build.lua [-label]")
+        luabc.debug.log.err("usage: lua build.lua [-label]")
     end
+
+    if is_clear then luabc.bind = { ["__default"] = {} } end
 end
 
-function luabc.cmd:new(label, order)
+--===================================================================================
+--================================================================= luabc.cmd
+--===================================================================================
+
+-- @brief Create a command object
+-- @param label Which label the cmd belongs to
+-- @param order Defines the execution order (ascending) among all the commands in the same label
+-- @param code_first Execute the code first or not
+-- @note The command must be associated with only one label (default label: '__default')
+function luabc.cmd:new(label, order, code_first)
     label = label or "__default"
     order = order or 1
-    local object = { args = {}, label = label, order = order }
+    code_first = code_first or false
+    local object = { 
+        _args       = {},           -- contains the parameters for the command line
+        _code       = {},           -- contains a function and its parameters
+        _link       = {},           -- contains a group of labels to be executed after this command (executed in order)
+        _label      = label,        -- the label associated with this command
+        _order      = order,        -- the execution order of this command within its label (executed in descending order)
+        _code_first = code_first    -- indicates whether the function should be executed first
+    }
     setmetatable(object, { __index = luabc.cmd })
     object:bind(label)
     return object
 end
 
+-- @brief Append the arg to 'cmd.args'
+-- @param ... Variable args
+-- @note There are two types of append: static and dynamic.
+-- In static append, the argument passed is constant and cannot be modified
+-- In dynamic append, the argument is a table, allowing modifications before execution
+-- as luabc will replace it with a string during runtime
 function luabc.cmd:append(...)
     local args = {...}
     if #args == 0 then
-        log.err("there has no args")
+        luabc.debug.log.err("there has no args")
         return
     end
     for _, arg in ipairs(args) do
-        if type(arg) == "table" then
-            arg = table.concat(arg, " ")
+        table.insert(self._args, arg)
+    end
+end
+
+-- @brief Appends the function to 'cmd.code' (this is why 'code_first' is required in cmd:new())
+-- @param func The function to be assigned to 'cmd'
+-- @param ... The parameters for the function
+-- @note Only one function can be assigned to 'cmd.code' at a time
+function luabc.cmd:call(func, ...)
+    local code = {func, ...}
+    if #code == 0 or type(func) ~= "function" then
+        luabc.debug.log.err("you should pass the function and its parameter")
+        return
+    end
+    self._code = code
+end
+
+-- @brief Remove the label from cmd.link
+-- @param label The removed one
+function luabc.cmd:unlink(label)
+    for idx, link_label in ipairs(self._link) do
+        if link_label == label then
+            table.remove(self._link, idx)
+            break
         end
-        table.insert(self.args, arg)
     end
 end
 
-function luabc.cmd:print(is_cmd_line_form)
-    if not is_cmd_line_form then
-        for key, val in ipairs(self.args) do print(key, val) end
-    else
-        print(table.concat(self.args, " "))
-    end
+-- @brief Add the label to cmd.link
+-- @param label The add one
+function luabc.cmd:link(label)
+    table.insert(self._link, label)
 end
 
+-- @brief Clear the cmd object
 function luabc.cmd:clear()
-    self.args = {}
+    self._args       = {}
+    self._code       = {}
+    self._link       = {}
+    self._label      = "__default"
+    self._order      = 1
+    self._code_first = false
 end
 
-function luabc.cmd:run()
-    if #(self.args) == 0 then return end
-    local cmd = table.concat(self.args, " ")
+local function exec_cmd(args)
+    if #(args) == 0 then return end
+    -- replace the variable to real value
+    local real_args = {}
+    for idx, arg in ipairs(args) do
+        arg = (type(arg) == "table") and table.concat(arg, " ") or arg
+        table.insert(real_args, arg)
+    end
+    local cmd = table.concat(real_args, " ")
     local status, msg = os.execute(cmd)
     if status then
-        log.ok("run successfully [ " .. cmd .. " ]")
+        luabc.debug.log.ok("run successfully [ " .. cmd .. " ]")
     else
-        log.err("run failed [ " .. cmd .. " ] : " .. msg)
+        luabc.debug.log.err("run failed [ " .. cmd .. " ] : " .. msg)
     end
-    self:clear()
 end
 
+local function exec_code(code)
+    if #(code) < 1 then return end
+    local status, result = pcall(code[1], select(2, table.unpack(code)))
+    if status then
+        luabc.debug.log.ok("call successfully")
+    else
+        luabc.debug.log.err("call failed: " .. result) 
+    end
+end
+
+local function exec_link(link)
+    for _, label in ipairs(link) do
+        for _, cmd in ipairs(luabc.bind[label]) do cmd:run() end
+    end
+end
+
+-- @brief Run the command
+-- @param is_clear Clear the old records after build or not (for debugging)
+function luabc.cmd:run(is_clear)
+    if self._code_first then
+        exec_code(self._code)
+        exec_cmd(self._args)
+        exec_link(self._link)
+    else
+        exec_cmd(self._args)
+        exec_code(self._code)
+        exec_link(self._link)
+    end
+
+    if is_clear == nil then is_clear = false else is_clear = is_clear and true end
+    if is_clear then self:clear() end
+end
+
+-- @brief Bind the cmd to a label
+-- @param label The binded one
 function luabc.cmd:bind(label)
     if not label then
-        log.info("you have to bind the command to an label")
+        luabc.debug.log.info("you have to bind the command to an label")
         return
     end
 
@@ -116,7 +197,7 @@ function luabc.cmd:bind(label)
 
     for key, val in pairs(luabc.bind) do
         -- free the binding of command and old label
-        if not free_flag and key == self.label then
+        if not free_flag and key == self._label then
             for idx, cmd in ipairs(val) do 
                 if cmd == self then
                     table.remove(val, idx) 
@@ -128,15 +209,24 @@ function luabc.cmd:bind(label)
         -- add the binding of command and new label
         if not add_flag and key == label then
             table.insert(val, self)
-            table.sort(val, function (a, b) return a.order < b.order end)
+            table.sort(val, function (a, b) return a._order < b._order end)
             add_flag = true
         end
 
         if free_flag and add_flag then break end
     end
-    self.label = label
+    self._label = label
 end
 
+--===================================================================================
+--================================================================= luabc.tool
+--===================================================================================
+
+-- @brief Searches for files with the specified extension in the given directory
+-- @param extension The file extension to match (e.g., 'txt', there has no dot here)
+-- @param dir The directory to search in
+-- @param recursive A boolean value indicating whether the search should be recursive
+-- @return A list of files that match the extension and the total number of matched files
 function luabc.tool.match_file_extension(extension, dir, recursive)
     dir = dir or "."
     extension = extension and ("*." .. extension) or "*"
@@ -154,15 +244,19 @@ function luabc.tool.match_file_extension(extension, dir, recursive)
     handle:close()
 
     if #res == 0 then
-        log.info("no matched files with extension [" .. extension .. "]")
+        luabc.debug.log.info("no matched files with extension [" .. extension .. "]")
     end
 
     return res, #res
 end
 
+-- @brief Replaces the file extension of the given filename with a new extension
+-- @param filename The original filename whose extension is to be replaced
+-- @param new_extension The new extension to replace the old one (e.g., 'jpg', there has no dot here)
+-- @return The filename with the new extension
 function luabc.tool.replace_file_extension(filename, new_extension)
     if not new_extension or not filename then
-        log.err("parameter <filename> and <new_extension> must be valid")
+        luabc.debug.log.err("parameter <filename> and <new_extension> must be valid")
         return
     end
     new_extension = "." .. new_extension
@@ -170,6 +264,23 @@ function luabc.tool.replace_file_extension(filename, new_extension)
     return new_filename
 end
 
+-- @brief Replaces the file extension of each file in the given list with a new extension
+-- @param files A list of filenames whose extensions are to be replaced
+-- @param new_extension The new extension to replace the old ones (e.g., 'jpg', there has no dot here)
+-- @return A list of filenames with the new extension applied to each
+function luabc.tool.replace_files_extension(files, new_extension)
+    if type(files) ~= "table" then
+        luabc.debug.log.err("the files should be a table")
+    end
+    local res = {}
+    for _, file in ipairs(files) do
+        table.insert(res, luabc.tool.replace_file_extension(file, new_extension))
+    end
+    return res
+end
+
+-- @brief Deletes the specified file or files
+-- @param file A single filename or a list of filenames to be deleted
 function luabc.tool.clean(file)
     if type(file) == "table" then
         file = table.concat(file, " ")
@@ -177,9 +288,86 @@ function luabc.tool.clean(file)
     local cmd = "rm -rf " .. file
     local status, msg = os.execute(cmd)
     if status then
-        log.ok("run successfully [ " .. cmd .. " ]")
+        luabc.debug.log.ok("run successfully [ " .. cmd .. " ]")
     else
-        log.err("run failed [ " .. cmd .. " ] : " .. msg)
+        luabc.debug.log.err("run failed [ " .. cmd .. " ] : " .. msg)
+    end
+end
+
+--===================================================================================
+--================================================================= luabc.debug
+--===================================================================================
+
+function luabc.debug.log.err(msg)
+    error("[" .. color.RED .. "ERROR" .. color.RESET .. "] " .. msg)
+end
+
+function luabc.debug.log.ok(msg)
+    print("[" .. color.GREEN .. "OK" .. color.RESET .. "] " .. msg)
+end
+
+function luabc.debug.log.info(msg)
+    print("[" .. color.CYAN .. "INFO" .. color.RESET .. "] " .. msg)
+end
+
+-- @brief Dumps the contents of an object, for debugging purposes
+-- @param obj The object to be dumped, which can be a simple or nested table
+-- @param level The depth level to which the object should be dumped (for nested tables)
+-- @note Allow nested tables
+function luabc.debug.dump(obj, level)
+    if not obj then 
+        luabc.debug.log.err("invalid obj to be dumpped")
+        return
+    end
+
+    level = level or 1
+    local indent = string.rep("    ", level)
+    if type(obj) ~= "table" then
+        print(indent .. tostring(obj))
+        return
+    end
+    
+    for key, value in pairs(obj) do
+        local formatted_key = tostring(key)
+        if type(value) == "table" then
+            print(indent ..  formatted_key .. " = {")
+            luabc.debug.dump(value, level + 1)
+            print(indent .. "}")
+        else
+            print(indent .. formatted_key .. " = " .. tostring(value))
+        end
+    end
+end
+
+-- @brief Prints the contents of an object. If the object is a table, it concatenates the table elements into a string
+-- @param obj The object to be printed. If it’s a table, its elements are concatenated
+-- @param is_concat A boolean that determines whether the table elements should be concatenated (true) or printed individually (false)
+-- @note Only table elements that can be converted to strings are allowed, and only one level of nesting is supported
+function luabc.debug.print(obj, is_concat)
+    if not obj then 
+        luabc.debug.log.err("invalid obj to be print")
+        return
+    end
+
+    if is_concat == nil then is_concat = false else is_concat = is_concat and true end
+    if type(obj) == "table" then
+        if not is_concat then
+            for key, val in ipairs(obj) do 
+                print(key, (type(val) == "table") and table.unpack(val) or val)
+            end
+        else
+            local res = ""
+            for key, val in ipairs(obj) do 
+                if type(val) == "table" then
+                    res = res .. table.concat(val)
+                else
+                    res = res .. val
+                end
+            end
+            print(res)
+        end
+    else
+        print(obj)
     end
 end
 
